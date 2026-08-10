5e0997d83ff0312af825fe277dde6398528305a2
Expertsteak <ff.loover150@gmail.com>
2026-08-10 09:28:15 +0530

    location

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -398,9 +398,22 @@ function Contact() {
 
       </section>
 
-      {/*LOCATION*/}
+      {/*LOCATION / MAP*/}
 
-      {/* gotta add the  location */}
+      <section className="map">
+
+       <h2>Find Us</h2>
+
+      <iframe
+         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.4015722418203!2d78.38577307591044!3d17.392505702656198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9d108f23a19f%3A0xacf7e85eb2aa896c!2sNurture%20and%20Grow%20primary%20school!5e0!3m2!1sen!2sin!4v1786333910065!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"
+         width="100%"
+         height="400"
+         style={{ border: 0 }}
+         allowFullScreen
+         loading="lazy"
+         title="Nurture & Grow Primary School Location"
+      ></iframe>
+      </section>
     </>
   );
 }
